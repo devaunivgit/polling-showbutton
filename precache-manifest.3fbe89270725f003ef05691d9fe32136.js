67d5097475bff102c3abb4aedd7dca02eb671a8f
Devanadhen <devauniv@gmail.com>
2020-06-19 14:02:55 +0530

    Updates

---
+++ precache-manifest.3fbe89270725f003ef05691d9fe32136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b6ea8357ffa9e9ad7c67b7fb7e5f59",
+    "url": "test.com/index.html"
+  },
+  {
+    "revision": "75b589d77adfe9f6b118",
+    "url": "test.com/static/css/2.0e1a4c73.chunk.css"
+  },
+  {
+    "revision": "9f4b898ea2789b2dadfa",
+    "url": "test.com/static/css/main.07a718d4.chunk.css"
+  },
+  {
+    "revision": "75b589d77adfe9f6b118",
+    "url": "test.com/static/js/2.f67f9d69.chunk.js"
+  },
+  {
+    "revision": "9f4b898ea2789b2dadfa",
+    "url": "test.com/static/js/main.2e641e5d.chunk.js"
+  },
+  {
+    "revision": "875b668495da0b72db0a",
+    "url": "test.com/static/js/runtime~main.9c3a70e1.js"
+  }
+]);
